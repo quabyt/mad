6e3741fc4abb3d5199ea5559dcc172aa037bcf8d
Hasan Gül <hsngl695@gmail.com>
2024-10-20 12:34:40 +0300

    week3

---
+++ week3/app/src/main/java/tr/edu/mu/ceng/mad/week3/Post.java
@@ -0,0 +1,37 @@
+package tr.edu.mu.ceng.mad.week3;
+
+import android.graphics.Bitmap;
+import android.location.Location;
+
+public class Post {
+    private String message;
+    private Location location;
+    private Bitmap image;
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+
+}
